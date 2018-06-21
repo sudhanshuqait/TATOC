ca3975501de0ac74d34e6e5451914fd61954cb7e
sudhanshuqait <40339109+sudhanshuqait@users.noreply.github.com>
2018-06-22 00:13:50 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		tatoc obj = new tatoc();
+		obj.tatoc();
+		obj.greenbox();
+		obj.dungeonnbox();
+		obj.draganddrop();
+		obj.popup();
+		obj.cookies();
+		
+		
+		
+
+	}
+
+}
---
+++ tatoc.java
@@ -0,0 +1,76 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import java.util.Iterator;
+import java.util.Set;
+import org.openqa.selenium.Cookie;
+ 
+public class tatoc {
+	
+public WebDriver driver=new ChromeDriver();
+
+public void tatoc() {
+driver.get("http://10.0.1.86/tatoc");
+driver.findElement(By.xpath("/html/body/div/div[2]/a[1]")).click();
+}
+
+public void greenbox() {
+	
+driver.findElement(By.className("greenbox")).click();
+}
+
+public void dungeonnbox() {
+driver.switchTo().frame(0);
+WebElement box1 = driver.findElement(By.id("answer"));
+String Box1 = box1.getAttribute("class");
+String Box2 = "";
+while(!Box1.equals(Box2)){
+  
+   driver.switchTo().defaultContent();
+    driver.switchTo().frame(0);
+    driver.findElement(By.cssSelector("a")).click();
+    driver.switchTo().frame(0);
+    Box2=driver.findElement(By.id("answer")).getAttribute("class");
+     }
+driver.switchTo().defaultContent();
+driver.switchTo().frame(0).findElement(By.linkText("Proceed")).click();
+}
+
+ public void draganddrop() {
+Actions act=new Actions(driver);
+WebElement drop = driver.findElement(By.cssSelector("#dropbox"));
+WebElement drag = driver.findElement(By.cssSelector("#dragbox"));
+act.dragAndDrop(drag, drop).build().perform();
+driver.findElement(By.linkText("Proceed")).click();
+ }
+
+ public void popup() {
+driver.findElement(By.linkText("Launch Popup Window")).click();
+String MainWindow=driver.getWindowHandle();
+String subWindow = null;
+Set<String> windows = driver.getWindowHandles();
+Iterator itr = windows.iterator();
+while(itr.hasNext()){
+	subWindow = (String) itr.next();
+}
+driver.switchTo().window(subWindow);
+driver.findElement(By.id("name")).sendKeys("TATOC");
+driver.findElement(By.id("submit")).click();
+driver.switchTo().window(MainWindow);
+driver.findElement(By.linkText("Proceed")).click();
+ }
+ 
+ public void cookies() {
+	 driver.findElement(By.linkText("Generate Token")).click();
+		String Cookie_val = driver.findElement(By.id("token")).getText();
+		Cookie ck = new Cookie("Token", Cookie_val.substring(7));
+		driver.manage().addCookie(ck);
+		driver.findElement(By.linkText("Proceed")).click();
+ }
+
+
+}
+ 
